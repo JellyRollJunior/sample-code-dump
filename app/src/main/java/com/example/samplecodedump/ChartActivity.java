940bf44d9396869e8e33e1c2789e4c101256fbdd
Brandon Lin <brandonlin9716@gmail.com>
2021-10-14 13:10:06 -0700

    Write notes and fix warnings.

--- app/src/main/java/com/example/samplecodedump/ChartActivity.java
+++ app/src/main/java/com/example/samplecodedump/ChartActivity.java
@@ -18,10 +18,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.ui.AppBarConfiguration;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class ChartActivity extends AppCompatActivity {
     float[] articles = {10.0f, 20.0f, 30.0f, 5.0f, 15.0f, 25.0f};
@@ -32,36 +31,18 @@ public class ChartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
 
+        // Sample Twenty Two
+        setupPieChart();
+
+        // Sample Twenty Three
+        sampleTwentyThreeAppBar();
+    }
+
+    private void sampleTwentyThreeAppBar() {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.myToolbar);
         setSupportActionBar(myToolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
-
-        // Sample Twenty Two
-        setupPieChart();
-    }
-
-    private void setupPieChart() {
-        // populate list of pie entries
-        List<PieEntry> pieEntries = new ArrayList<>();
-        for (int i = 0; i < articles.length; i++) {
-            pieEntries.add(new PieEntry(articles[i], articleNames[i]));
-        }
-
-        PieDataSet dataSet = new PieDataSet(pieEntries, getString(R.string.chart_title_my_clothing));
-        // Color Template is the provided default template (can create your own)
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        PieData data = new PieData(dataSet);
-
-        // get the chart
-        PieChart chart = (PieChart) findViewById(R.id.chartSampleTwentyTwo);
-        chart.setData(data);
-        chart.animateY(1000);
-        chart.invalidate();     // make chart redraw
-    }
-
-    public static Intent makeIntent(Context context) {
-        return new Intent(context, ChartActivity.class);
     }
 
     @Override
@@ -87,5 +68,28 @@ public class ChartActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void setupPieChart() {
+        // populate list of pie entries
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for (int i = 0; i < articles.length; i++) {
+            pieEntries.add(new PieEntry(articles[i], articleNames[i]));
+        }
+
+        PieDataSet dataSet = new PieDataSet(pieEntries, getString(R.string.chart_title_my_clothing));
+        // Color Template is the provided default template (can create your own)
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataSet);
+
+        // get the chart
+        PieChart chart = (PieChart) findViewById(R.id.chartSampleTwentyTwo);
+        chart.setData(data);
+        chart.animateY(1000);
+        chart.invalidate();     // make chart redraw
+    }
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context, ChartActivity.class);
     }
 }
