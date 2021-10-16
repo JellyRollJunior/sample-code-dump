c5d7bfc7c3f5aa1d27aa7b557135c9ccddf7fbb1
Brandon Lin <brandonlin9716@gmail.com>
2021-10-15 19:53:41 -0700

    Singleton practice implementation.

--- app/src/main/java/com/example/samplecodedump/StringHolder.java
+++ app/src/main/java/com/example/samplecodedump/StringHolder.java
@@ -6,6 +6,16 @@ public class StringHolder {
     private final String second;
     private final String third;
 
+    // Sample 27: singleton practice
+    private static StringHolder instance;
+
+    public static synchronized StringHolder getInstance(String first, String second, String third) {
+        if (instance == null) {
+            instance = new StringHolder(first, second, third);
+        }
+        return instance;
+    }
+
     public StringHolder(String first, String second, String third) {
         this.first = first;
         this.second = second;
